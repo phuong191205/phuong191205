ada7038ac9fd3118afd10e115899970b96055e4c
MFuong-cloud <phuongtmph51146@gmail.com>
2026-02-03 10:42:04 +0700

    đăng kí đăng nhập

--- src/app/components/signup/signup.ts
+++ src/app/components/signup/signup.ts
@@ -14,13 +14,19 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class Signup {
   email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
   submitted = false;
+  showPassword = false;
 
   onSubmit(event: Event) {
     event.preventDefault();
     this.submitted = true;
-    if (this.email.valid) {
-      console.log('Signup form submitted', this.email.value);
+    if (this.email.valid && this.password.valid) {
+      console.log('Signup form submitted', { email: this.email.value, password: this.password.value });
     }
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
 }
